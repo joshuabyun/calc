983bdb8f35c425f42193d9c88d5193c5c1183d12
Joshua Byun <byunsung89@gmail.com>
2016-09-25 12:50:56 -0700

    refactored if statements with swich, added display functions, added ng-keyup for keyboard press. needs clear and clear entry condition

--- script.js
+++ script.js
@@ -1,35 +1,44 @@
 var app = angular.module('calcApp', []);
 app.controller('calcController',function($log){
-    this.calcExpressionDisplay;
-    this.inputDisplay;
-    this.inputArray = [];//array of objects { value : "", type : "" }
+    this.upperDisplay = "";
+    this.lowerDisplay;
+    this.inputArray = [];
     this.equalSignClicked = false;
     this.ceClicked = false;
 
-
-    var createObj = function(clickedInput,inputType,self){
-        var inputObj = new Object();
-        inputObj["value"] = clickedInput;
-        inputObj["type"] = inputType;
-        self.inputArray.push(inputObj);
+    // this.console = function(input){
+    //   console.log(input);  
+    // };
+    this.updateLowerDisplay = function(){
+        var lastNumPos = this.inputArray.length-1;
+        this.lowerDisplay = this.inputArray[lastNumPos].value;
     };
-
-    //checks for the input type, calls approperiate functions to keep track of input
-    //calls calculate, clear, records num/operator
+    this.updateUpperDisplay = function(){
+      for(var obj in this.inputArray){
+          this.upperDisplay += this.inputArray[obj].value;
+      }
+    };
+    this.clearUpperDisplay = function(){
+      this.upperDisplay = "";
+    };
+    //checks for the input type, calls approperiate functions according to the input
     this.acceptClickedKey = function(clickedInput,inputType){
         var self = this;
-        if(inputType == 'numKey' || inputType == 'specialChar'){
-            //call check for calculate condition
-            this.checkCalcEligible(clickedInput,inputType);
-
-            if(this.validateInput(clickedInput,inputType)){
-                if(inputType == "specialChar" || this.inputArray.length == 0){ //first input item/operator condition
+        this.validateInput(clickedInput,inputType); //validates input - returns False on multiple decimals in one number, consecutive operators, operator as a first input
+        switch(inputType){
+            case 'numKey' :
+                if(this.inputArray.length == 0){
                     createObj(clickedInput,inputType,self);
-                    //call update display -for upper and lower display
                 }else{
                     var arrayLastPosition = this.inputArray.length -1;
                     if(this.inputArray[arrayLastPosition].type == "numKey"){ //consecutive number input condition
-                        this.inputArray[arrayLastPosition].value += clickedInput;
+                        if(this.equalSignClicked){
+                            this.inputArray = [];
+                            createObj(clickedInput,inputType,self);
+                            this.equalSignClicked = false;
+                        }else{
+                            this.inputArray[arrayLastPosition].value += clickedInput;
+                        }
                         //call update display -for lower display only
                     }
                     else{
@@ -37,38 +46,56 @@ app.controller('calcController',function($log){
                         //call update display -for upper and lower display
                     }
                 }
-            }
-        }else{
-            if(inputType == "equalSign"){ //equal sign condition
-                var sum = this.doMath(parseFloat(this.inputArray[0].value),parseFloat(this.inputArray[2].value),this.inputArray[1].value); //would like to use a ref value instead of a hard number
-                createObj(sum,"numKey",self);
-                this.inputArray.splice(0,this.inputArray.length-1);
-                this.equalSignClicked = true;
-                //call update display -for upper and lower display - needs to clear upper display
-                $log.info("final value : ", sum);
-            }else{
-                if(clickedInput == "C"){//clear all condition
+                this.updateLowerDisplay();
+                break;
+            case 'specialChar' :
+                this.equalSignClicked = false;
+                if(this.checkCalcEligible()){
+                    var calculatedNum = this.doMath(parseFloat(this.inputArray[0].value),parseFloat(this.inputArray[2].value),this.inputArray[1].value); //would like to use a ref value instead of a hard number
                     this.inputArray = [];
-                    //call update display -for upper and lower display
-                }else{ //clear entry condition
-                    var lastArrayPos = this.inputArray.length-1;
-                    if(this.ceClicked || this.inputArray.length == 0){ //need to deactivate after new input has been inputted
-                        $log.warn('invalid');
-                        return;
-                    }
-                    else{
-                       
-                    }
+                    createObj(calculatedNum,"numKey",self);
+                    createObj(clickedInput,inputType,self);
+                }else{
+                    createObj(clickedInput,inputType,self);
                 }
-            }
-            //need to take account for clear buttons
+                this.updateUpperDisplay();
+                break;
+            case 'equalSign' :
+                if(this.checkCalcEligible()){
+                    var output = this.doMath(parseFloat(this.inputArray[0].value),parseFloat(this.inputArray[2].value),this.inputArray[1].value); //would like to use a ref value instead of a hard number
+                    createObj(output,"numKey",self);
+                    this.inputArray.splice(0,this.inputArray.length-1);
+                    this.equalSignClicked = true;
+                    $log.info("final value : ", output);
+                    this.clearUpperDisplay();
+                    this.updateLowerDisplay();
+                }
+                break;
+            //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+            case 'clear' :
+                // if(clickedInput == "C"){//clear all condition
+                //     this.inputArray = [];
+                //     //call update display -for upper and lower display
+                // }else{ //clear entry condition
+                //     var lastArrayPos = this.inputArray.length-1;
+                //     if(this.ceClicked || this.inputArray.length == 0){ //need to deactivate after new input has been inputted
+                //         $log.warn('invalid');
+                //         return;
+                //     }
+                //     else{
+                //
+                //     }
+                // }
+                // //need to take account for clear buttons
+                break;
         }
-        $log.log('updated inputArray',this.inputArray);
+        $log.log(this.inputArray);
     };
+
 
     //checks for consecutive operators and multiple decimal points in an input : returns boolean
     this.validateInput = function(clickedInput,inputType){
-        //first input condition
+        //first input condition - only looks for the number
         if(this.inputArray.length == 0){
             if(inputType == "specialChar"){
                 $log.error('first input cannot be operator');
@@ -84,7 +111,7 @@ app.controller('calcController',function($log){
                 return false;
             }else{
                 if(clickedInput =="." && this.inputArray[arrayLastPosition].value.indexOf(".") != -1){
-                    $log.error('cannot multiple decimal points in a number');
+                    $log.error('cannot use multiple decimals in a number');
                     return false;
                 }else{
                     return true;
@@ -92,25 +119,24 @@ app.controller('calcController',function($log){
             }
         }
     };
-
-    //checks whether the expression is ready for calculation
-    this.checkCalcEligible = function(clickedInput,inputType){
-        var self = this;
-        if(inputType == "specialChar"){
-            for(var input in this.inputArray){
-                 if(this.inputArray[input].type == "specialChar"){
-                     var calculatedNum = this.doMath(parseFloat(this.inputArray[0].value),parseFloat(this.inputArray[2].value),this.inputArray[1].value); //would like to use a ref value instead of a hard number
-                     //this.inputArray = []; //////////////////////////////////////////////////////clear array and create new obj :doesn't work
-                     createObj(calculatedNum,"numKey",self);///////////////////////////////////////
-                     this.inputArray.splice(0,3);/////////////////////////////////////////////////create new obj and clear array : works ...why?
-                 }
+    //creates an object to insert into inputArray
+    var createObj = function(clickedInput,inputType,self){
+        var inputObj = new Object();
+        inputObj["value"] = clickedInput;
+        inputObj["type"] = inputType;
+        self.inputArray.push(inputObj);
+    };
+    //checks whether the expression is ready to calculate
+    this.checkCalcEligible = function(){
+        if(this.inputArray.length == 3){
+            if(this.inputArray[0].type == "numKey" && this.inputArray[2].type == "numKey" && this.inputArray[1].type == "specialChar"){
+                return true;
+            }else{
+                $log.error('incomplete expression');
             }
         }
     };
-
-
-
-
+    //actual calculation
     this.doMath = function(num1, num2, operator){
         $log.log(num1, num2, operator);
         //check for all params. if not available through error
